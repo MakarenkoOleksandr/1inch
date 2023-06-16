a10ff39f553317c5e1a4c7bc19b88ac6b4006ce9
MakarenkoOleksandr <majop11111@gmail.com>
2023-06-16 21:12:59 +0300

    Перезалил репозиторий

--- main_1inch.py
+++ main_1inch.py
@@ -10,7 +10,4 @@ wallet_address = os.getenv('PRIVATE_ADDRESS')
 network, start_token, end_token, amount = get_inputs()
 get_tokens_API(network)
 start_address, end_address = get_token_address(start_token, end_token)
-# swap_tokens()
-swap_tokens(start_address, end_address, wallet_address, amount)
-print(amount)
-# python main_1inch.py --network Arbitrum --start_token "Tether USD" --end_token Ethereum --amount 1
+# python main_1inch.py --network "BNB Chain" --start_token "BNB" --end_token DAI --amount 0.001
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 private_key = os.getenv('PRIVATE_KEY')
 rpc_url = os.getenv('RPC_URL')
+private_address = os.getenv('PRIVATE_ADDRESS')
 
 web3 = Web3(Web3.HTTPProvider(rpc_url))
 
@@ -17,41 +18,54 @@ else:
 
 
 def get_contract():
-    url = 'https://api.1inch.io/v5.0/42161/swap?fromTokenAddress=0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9&toTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&amount=1&fromAddress=0xb2D7032421e1ff894b14Dc9384A7Cc2Cc312e5AD&slippage=1&disableEstimate=true&burnChi=false&allowPartialFill=false'
+    url = 'https://api.1inch.io/v5.0/56/swap'
 
-    response = requests.get(url)
+    params = {
+        "fromTokenAddress": "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",  # BNB token address
+        "toTokenAddress": "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3",  # DAI token address
+        "amount": 0.001,  # Amount of BNB to swap
+        "fromAddress": private_address,  # Your wallet address
+        "slippage": 1,
+        "disableEstimate": True,
+        "burnChi": False,
+        "allowPartialFill": False
+    }
+    response = requests.get(url, params=params)
     response_data = response.json()
+    print(response_data)
+#     to_token_amount = response_data['toTokenAmount']
+#     from_token_address = response_data['fromToken']['address']
+#     to_token_address = response_data['toToken']['address']
 
-    from_token_amount = response_data['fromTokenAmount']
-    from_token_address = response_data['fromToken']['address']
-    to_token_address = response_data['toToken']['address']
+#     tx_to = Web3.to_checksum_address(response_data['tx']['to'])
 
-    tx_to = Web3.to_checksum_address(response_data['tx']['to'])
+#     sender_address = response_data['tx']['from']
+#     nonce = web3.eth.get_transaction_count(sender_address)
 
-    sender_address = response_data['tx']['from']
-    nonce = web3.eth.get_transaction_count(sender_address)
+#     with open('erc20.abi.json') as f:
+#         abi = json.load(f)
+#     token_contract = web3.eth.contract(address=tx_to, abi=abi)
 
-    with open('erc20.abi.json') as f:
-        abi = json.load(f)
-    token_contract = web3.eth.contract(address=tx_to, abi=abi)
+#     swap_tx = token_contract.functions.transfer(
+#         Web3.to_checksum_address(to_token_address),
+#         to_token_amount
+#     ).build_transaction({
+#         'from': sender_address,
+#         'nonce': nonce,
+#         'value': 0,
+#         'gas': 1000000,
+#         'gasPrice': 100000000
+#     })
 
-    approve_tx = token_contract.functions.transferFrom(from_token_address, to_token_address, sender_address, from_token_amount).build_transaction({
-        'from': sender_address,
-        'nonce': nonce,
-        'value': 1,
-        'gas': 1000000,
-        'gasPrice': 100000000
-    })
+#     swap_signed_tx = web3.eth.account.sign_transaction(
+#         swap_tx, private_key=private_key)
+#     swap_tx_hash = web3.eth.send_raw_transaction(
+#         swap_signed_tx.rawTransaction)
+#     web3.eth.wait_for_transaction_receipt(swap_tx_hash)
 
-    approve_signed_tx = web3.eth.account.sign_transaction(
-        approve_tx, private_key=private_key)
-    approve_tx_hash = web3.eth.send_raw_transaction(
-        approve_signed_tx.rawTransaction)
-    web3.eth.wait_for_transaction_receipt(approve_tx_hash)
-
-    receipt = web3.eth.wait_for_transaction_receipt(approve_tx_hash)
-    print("Транзакция успешно выполнена")
-    print("Receipt:", receipt)
+#     receipt = web3.eth.wait_for_transaction_receipt(swap_tx_hash)
+#     print("Транзакция успешно выполнена")
+#     print("Receipt:", receipt)
 
 
 get_contract()
