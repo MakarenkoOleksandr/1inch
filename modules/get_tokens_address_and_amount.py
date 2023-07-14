2066a3bb90e3cd7a0cae698c7c19199d1cf2cad6
MakarenkoOleksandr <majop11111@gmail.com>
2023-07-14 14:27:12 +0300

    Дополнил условия ввода ссумы обмена

--- modules/get_tokens_address_and_amount.py
+++ modules/get_tokens_address_and_amount.py
@@ -30,11 +30,13 @@ def check_from_tokens_input(token_input):
         
     print('Warning! You have entered a wrong send token. Try again: ')
     return get_from_tokens_input()
-    
+
+
 def get_to_tokens_input():
     to_token_input = input('Token to receive: ').replace(',', ' ').upper()
     correct_to_token_input = check_to_tokens_input(to_token_input)
     return correct_to_token_input
+
 
 def check_to_tokens_input(token_input):
     for token in tokens:
@@ -45,20 +47,37 @@ def check_to_tokens_input(token_input):
     print('Warning! You have enter wrong receive token. Try again: ')
     token_address = get_to_tokens_input()
 
-
-def get_tokens_address_and_amount_for_swap():
-    from_token_address = get_from_tokens_input()
-    to_token_address = get_to_tokens_input()
-                 
+def get_amount_input(from_token_address):
     input_amount = input('Enter amount for swap: ')
+    correct_input_amount = check_amount_input(input_amount, from_token_address)
+    return correct_input_amount
+
+def check_amount_input(input_amount, from_token_address):
+    while input_amount != 'max':
+        try:
+            amount = float(input_amount)
+            if 0 < amount < 1:
+                amount = int(Decimal(input_amount) * 10 ** 18)
+                break
+            elif amount >= 1:
+                amount = int(web3.to_wei(input_amount, 'ether'))
+                break
+        except ValueError:
+            print('Value must be a number. Try again')
+        
+        get_amount_input()
+
     if input_amount == 'max':
         token_contract = web3.eth.contract(address=web3.to_checksum_address(from_token_address), abi=token_abi)
         balance = token_contract.functions.balanceOf(wallet).call()
         amount = balance
-    if input_amount != 'max':
-        if float(input_amount) < 1:
-            amount = int(Decimal(input_amount) * 10 ** 18)
-        else:
-            amount = int(web3.to_wei(input_amount, 'ether'))    
+
+    return amount
+
+
+def get_tokens_address_and_amount_for_swap():
+    from_token_address = get_from_tokens_input()
+    to_token_address = get_to_tokens_input()                 
+    amount = get_amount_input(from_token_address)
 
     return from_token_address, to_token_address, amount
